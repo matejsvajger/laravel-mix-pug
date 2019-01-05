8ac9927d3ff6795e8f9eadd1d4e4d7ab66370abc
dai <chamestica@gmail.com>
2019-01-05 17:45:43 +0900

    Use Assert class instead of Verify in laravel-mix@>=2.x

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-const Verify = require('laravel-mix/src/Verify');
+let Verify, Assert;
+// laravel-mix@1.x
+try { Verify = require('laravel-mix/src/Verify'); }
+// laravel-mix@>=2.x
+catch (e) { Assert = require('laravel-mix/src/Assert'); }
+
 const notifier = require('node-notifier');
 const glob = require('glob');
 
 function pug(src, dest, options) {
 
-    Verify.dependency('pug', ['pug'], true);
-        
+    // laravel-mix@1.x
+    if (Verify != null) Verify.dependency('pug', ['pug'], true);
+    // laravel-mix@>=2.x
+    else Assert.dependencies(['pug'], true);
+
     let files = glob.sync(src);
 
     let MixPugTask = require('./MixPugTask');
