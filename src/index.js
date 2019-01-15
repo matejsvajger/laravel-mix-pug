828c1c81e38c497135f713e6c15afdc30fcb08e4
dai <chamestica@gmail.com>
2019-01-16 04:46:00 +0900

    Remove addAsset

--- src/index.js
+++ src/index.js
@@ -18,35 +18,6 @@ function pug(src, dest, options) {
 
     let MixPugTask = require('./MixPugTask');
 
-    // Changes to temporarily avoid the problem
-    // that this plugin can not be used with laravel-mix@>=4.0.0
-    // https://github.com/matejsvajger/laravel-mix-pug/issues/9
-    if (Mix.addAsset == null) {
-
-        // Alternative to Config.customAssets
-        // https://github.com/JeffreyWay/laravel-mix/blob/v3/src/config.js#L20
-        const configCustomAssets = [];
-
-        // Re-enable addAsset method for this plugin
-        // https://github.com/JeffreyWay/laravel-mix/blob/v3/src/Mix.js#L79
-        Mix.addAsset = function(asset) {
-            configCustomAssets.push(asset);
-        };
-
-        // Re-enable processing to add customAssets to manifest
-        // https://github.com/JeffreyWay/laravel-mix/blob/v3/src/Manifest.js#L66
-        const Manifest = Object.getPrototypeOf(Mix.manifest);
-        Manifest.transform = function(stats) {
-            let customAssets = configCustomAssets.map(asset =>
-                asset.pathFromPublic()
-            );
-            this.flattenAssets(stats)
-                .concat(customAssets)
-                .forEach(this.add.bind(this));
-            return this;
-        };
-    }
-
     Mix.addTask(new MixPugTask({
         files, dest, options
     }));
